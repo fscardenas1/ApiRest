cc9efdcc9008e0561dbe6013df678aed2d5370d9
Freddy Sebastian Cárdenas Rosero <fscardenas1@espe.edu.ec>
2023-08-23 19:13:50 -0500

    finalizado
    finalizado

---
+++ ApiRest/demo/src/main/java/com/example/ApiRest/controller/taskcontroller.java
@@ -0,0 +1,44 @@
+package com.example.ApiRest.controller;
+
+import com.example.ApiRest.model.Task;
+import com.example.ApiRest.service.TaskService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/tasks")
+public class TaskController {
+
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskService.getAllTasks();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Task> getTaskById(@PathVariable Long id) {
+        return taskService.getTaskById(id);
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return taskService.createTask(task);
+    }
+
+    @PutMapping("/{id}")
+    public Task updateTask(@PathVariable Long id, @RequestBody Task task) {
+        return taskService.updateTask(id, task);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
+    }
+}
---
+++ ApiRest/demo/src/main/java/com/example/ApiRest/repository/taskrepository.java
@@ -0,0 +1,9 @@
+package com.example.ApiRest.repository;
+
+import com.example.ApiRest.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
